d7b1718ffbab7b7c2e09a2508f32631786b0f6b4
Pauleg7 <p.s.84@mail.ru>
2019-04-29 23:51:17 +0300

    add 1? 2 task

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+console.log('it works!');
+
+export default function getSumm(value_1, value_2) {
+  let summ = value_1 + value_2;
+  return summ;
+}
---
+++ test/test.js
@@ -0,0 +1,8 @@
+import getSumm from "../src/js/app";
+
+test ('should summ', () => {
+  const recieved = getSumm(5, 5);
+  const expected = 10;
+
+  expect(recieved).toBe(expected);
+}); 
